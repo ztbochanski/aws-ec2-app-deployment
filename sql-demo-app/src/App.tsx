e95cbb6db8a9fcba2a97c8afeb627cf599bcbe9f
Zach Bochanski <ztbochanski@gmail.com>
2023-07-14 16:32:41 -0700

    add SQL . command demo

--- sql-demo-app/src/App.tsx
+++ sql-demo-app/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Demo from './components/demo';
+import SQLCommand from './components/sqlCommand';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>SQL Demo App</h1>
+      <SQLCommand />
       <Demo />
     </div>
   );
