55e3d1769ed1340d61b7d3a14182c23948d70fea
Zach Bochanski <ztbochanski@gmail.com>
2023-07-14 16:16:25 -0700

    refactor names, clean up

--- server/main/server-configuration.js
+++ server/main/server-configuration.js
@@ -1,10 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes')
+var indexRouter = require('./route-controller')
 
 var app = express();
 
@@ -14,19 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.use('/', indexRouter)
 
